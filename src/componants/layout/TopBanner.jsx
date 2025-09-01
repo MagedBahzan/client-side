60a7a89c59a5f1c7fce569936d1aba1089b0293a
maged bahzan <magedbahzan@gmail.com>
2025-09-01 16:31:56 +0300

    00

--- src/componants/layout/TopBanner.jsx
+++ src/componants/layout/TopBanner.jsx
@@ -17,7 +17,7 @@ function TopBanner() {
           className="w-6 h-6 top-banner-arrow"
         >
           <path
-            strokeLinecap="round"
+            strokeLinecap="round" 
             strokeLinejoin="round"
             d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
           />
