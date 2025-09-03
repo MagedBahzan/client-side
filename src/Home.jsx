3fd36628f40c69a903309e93f07e6717099c3993
maged bahzan <magedbahzan@gmail.com>
2025-09-03 12:47:46 +0300

    00

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,45 +3,8 @@ import Ads from "./componants/home-componants/Ads";
 import RecentNews from "./componants/home-componants/BreakingNews";
 
 function Home() {
-  const [theApiData, setTheApiData] = React.useState({});
-  let categoryEle = [];
-  React.useEffect(function loadDoc() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function () {
-      if (this.responseText === undefined) {
-        setTheApiData(null);
-      } else {
-        setTheApiData(JSON.parse(this.responseText).data.allDocs);
-      }
-    };
-    xhttp.open("GET", "http://127.0.0.1:8000/api/v1/main/news", true);
-    xhttp.send();
-  }, []);
-  // console.log(theApiData ? theApiData.allDocs : null);
-  // const firstDoc = theApiData.allDocs ? theApiData.allDocs[0] : null;
-  // console.log(firstDoc);
-  // console.log(theApiData ? theApiData.allDocs : null);
-
-  function categoryList() {
-    if (theApiData.length > 0 && theApiData) {
-      theApiData.map((val) =>
-        !categoryEle.includes(val.category)
-          ? categoryEle.push(val.category)
-          : null
-      );
-    }
-  }
-  categoryList()
-
-  console.log(categoryEle);
-  console.log(theApiData);
   return (
     <div className="home">
-      {/* <div className="joks--contaner">
-              {<pre>{JSON.stringify(theApiData, null, 2)}</pre>}
-              <p>{count}</p>
-              <button onClick={numCount}>Clic for Counting</button>
-          </div> */}
       <Ads />
       <RecentNews />
     </div>
--- src/componants/home-componants/BreakingNews.jsx
+++ src/componants/home-componants/BreakingNews.jsx
@@ -3,42 +3,72 @@ import "../home-desktop-styles/breakingNews.css";
 import "../home-laptop-styles/breakingNews.css";
 import "../home-phone-styles/breakingNews.css";
 import Template from "./Templat";
-import Courses from "../CoursesData";
-import { NavLink } from "react-router-dom";
+// import newsData from "../CoursesData";
+// import { NavLink } from "react-router-dom";
 
 function BreakingNews() {
-  const courseSlide = Courses.map((ele) => {
+
+  const [newsData, setTheApiData] = React.useState({});
+  let categoryEle = [];
+  React.useEffect(function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+      if (this.responseText === undefined) {
+        setTheApiData(null);
+      } else {
+        setTheApiData(JSON.parse(this.responseText).data.allDocs);
+      }
+    };
+    xhttp.open("GET", "http://127.0.0.1:8000/api/v1/main/news", true);
+    xhttp.send();
+  }, []);
+  
+  function categoryList() {
+    if (newsData.length > 0 && newsData) {
+      newsData.map((val) =>
+        !categoryEle.includes(val.category)
+          ? categoryEle.push(val.category)
+          : null
+      );
+    }
+  }
+  categoryList()
+
+  console.log(newsData);
+  console.log(newsData);
+
+  const newsSlide = newsData.length > 0 && newsData ? newsData.map((ele) => {
     return (
       <Template
         key={ele.id}
         paragraphClassName="courses-slids-paragraph"
-        courseImage={ele.courseImage}
+        courseImage={ele.imgSrc}
         courseImageClassName="courses-section-image"
-        coursTime={ele.coursTime}
-        courseLevel={ele.courseLevel}
-        profsoerName={ele.profsoerName}
-        heading={ele.heading}
-        paragriph={ele.paragriph}
+        coursTime={ele.author[0].name}
+        courseLevel={ele.category}
+        avatar={ele.author[0].avatar}
+        avatarClassName="avatarClassName"
+        paragriph={ele.description}
         slidsButtonContainerClassName="courses-slids-button-container"
         slidsButtonClassName="courses-slids-button"
-        buttonName="Get it now"
-        parm={`${ele.heading}`}
+        buttonName="Show more"
+        // parm={`${ele.heading}`}
         link="courses"
       />
     );
-  });
+  }) : null;
   return (
-    <div className="courses-section-container">
-      <div>
-        <div className="courses-top-header">
-          <p>Breaking news</p>
-        </div>
-        <div className="view-all-courses">
-          <NavLink to="courses">View All</NavLink>
-        </div>
-      </div>
-      <div className="courses-slides-container">{courseSlide}</div>
-    </div>
+  //   <div className="courses-section-container">
+  //     <div>
+  //       <div className="courses-top-header">
+  //         <p>Breaking news</p>
+  //       </div>
+  //       <div className="view-all-courses">
+  //         <NavLink to="courses">View All</NavLink>
+  //       </div>
+  //     </div>
+      <div className="courses-slides-container">{newsSlide}</div>
+  //   </div>
   );
 }
 
